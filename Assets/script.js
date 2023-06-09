3e6b54570b9eff343a9d0fa2f02368ee08988428
Conneroc03 <122755260+Conneroc03@users.noreply.github.com>
2023-06-08 20:04:15 -0600

    adding more to js

--- Assets/script.js
+++ Assets/script.js
@@ -2,3 +2,10 @@ const APIkey = "44c0ceeaad2f13d35c6b6f46923f9c42";
 let city = document.querySelector("#city").value;
 let lat, lon;
 
+const weatherEl = document.getElementById("weather");
+const fiveDayforecastEl = document.querySelectorAll(".fivedayforecast");
+const favoritesEl = document.getElementById("favorites");
+const weathercontainerEl = document.getElementsByClassName("weathercontainer")[0];
+const clearhistorybtn = document.querySelector("#clearhistorybtn");
+const clearfavoritesbtn = document.querySelector("#clearfavoritesbtn");
+const getweatherbtn = document.querySelector("#getweatherbtn");
